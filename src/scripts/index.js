709190a0e0813e0b31f5bc2f83d253798922c783
Erdeprof <rizkidarmawanrpl@gmail.com>
2021-07-19 17:00:21 +0700

    Commit update splitchunk optimization

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,42 @@
 import '../styles/main.css';
-// import filter from 'lodash.filter';
+import filter from 'lodash.filter';
 import contacts from './contacts';
-
+ 
 const inputContactForm = document.querySelector('#inputContactForm');
 const inputName = document.querySelector('#inputName');
 const inputNumber = document.querySelector('#inputNumber');
 const inputType = document.querySelector('#inputType');
-
+ 
 const showContactForm = document.querySelector('#showContactForm');
 const contactType = document.querySelector('#contactType');
-
+ 
 const contactContainer = document.querySelector('#contacts');
-
+ 
 inputContactForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const contact = {
-    id: new Date().valueOf()
-      .toString(),
-    name: inputName.value,
-    number: inputNumber.value,
-    type: inputType.value,
-  };
-  contacts.push(contact);
+ event.preventDefault();
+ const contact = {
+   id: new Date().valueOf()
+     .toString(),
+   name: inputName.value,
+   number: inputNumber.value,
+   type: inputType.value,
+ };
+ contacts.push(contact);
 });
-
+ 
 showContactForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  contactContainer.innerHTML = '';
-
-  // filter(contacts, contactType.value === 'all' ? {} : { type: contactType.value })
-    // .forEach(renderContact);
-    import('lodash.filter')
-    .then((module) => module.default)
-    .then(filterContacts)
-    .catch((error) => alert(error));
+ event.preventDefault();
+ contactContainer.innerHTML = '';
+ 
+ filter(contacts, contactType.value === 'all' ? {} : { type: contactType.value })
+   .forEach(renderContact);
 });
-
-const filterContacts = (filter) => {
-  filter(contacts, contactType.value === 'all' ? {} : { type: contactType.value })
-  .forEach(renderContact);
-};
-
+ 
 const renderContact = (contact) => {
-  contactContainer.innerHTML += `
-            <li>
-                <h4>${contact.name} (${contact.type})</h4>
-                <p>${contact.number}</p>
-            </li>
-   `;
+ contactContainer.innerHTML += `
+           <li>
+               <h4>${contact.name} (${contact.type})</h4>
+               <p>${contact.number}</p>
+           </li>
+  `;
 };
